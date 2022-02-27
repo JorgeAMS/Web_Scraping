8ea477665609cb8172bca418e1423d48ef6bce62
Jorge Morales <jamorales516@icloud.com>
2022-02-27 00:02:27 -0500

    script to get N random email addresses from web scraping

---
+++ random_email.py
@@ -0,0 +1,31 @@
+from selenium import webdriver 
+from selenium.webdriver.common.by import By 
+
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC 
+
+from selenium.webdriver.support.ui import Select
+
+chrome_driver_path = './chromedriver.exe' 
+options = webdriver.ChromeOptions() 
+options.headless = True 
+ 
+url = "https://theonegenerator.com/es/generadores/documentos/generador-de-email/" 
+
+data = 3
+ 
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+driver.get(url) 
+
+driver.set_page_load_timeout(150)
+
+for a in range(data):
+
+    element = driver.find_element(By.TAG_NAME, "form")
+    element.submit()
+
+    input = driver.find_element(By.ID, "copyToClipboard-email").get_attribute('value')
+
+    print(input)
+
+driver.quit()
