0ca5d9aa3cd205eb1b9b8a8da4259942aca79111
Jorge Morales <jamorales516@icloud.com>
2022-02-26 23:24:24 -0500

    script to get N random names from web scraping

---
+++ random_names.py
@@ -0,0 +1,25 @@
+from selenium import webdriver 
+from selenium.webdriver.common.by import By 
+
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC 
+
+chrome_driver_path = './chromedriver.exe' 
+options = webdriver.ChromeOptions() 
+options.headless = True 
+ 
+url = "https://generadordenombres.online/" 
+
+data = 3
+ 
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+driver.get(url) 
+
+for a in range(data):
+    
+    input = driver.find_element(By.ID, "resultadoGenerado").text
+    driver.execute_script("location.reload()")
+
+    print(input)
+
+driver.quit()
